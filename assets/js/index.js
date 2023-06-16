07206ea873a167841c7eb6220182f0f85cd5a48a
Erwan <erwanpellerinpro@gmail.com>
2023-06-16 16:55:49 +0200

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  try {
+    console.log("content loaded");
+
+    document.querySelector("#sign-up").addEventListener("click", () => {
+      console.log("connectez-vous");
+
+      document.querySelector("#modal").classList.remove("hidden");
+    });
+
+    document.querySelector("#close").addEventListener("click", () => {
+      console.log("fermer");
+
+      document.querySelector("#modal").classList.add("hidden");
+    });
+
+    document.querySelector("form").addEventListener("submit", async (event) => {
+      event.preventDefault();
+
+      const data = {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#message").value,
+      };
+
+      console.log(data);
+
+      const response = await axios.post(
+        "https://site--tripadvisor-backend--fwddjdqr85yq.code.run/form",
+        data
+      );
+      console.log(response);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
